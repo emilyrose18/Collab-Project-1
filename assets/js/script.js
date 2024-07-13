1623c4ab5b48bccf4cef7f1678ddf0a1cb28122f
Steve W <stephenw987@gmail.com>
2024-07-13 15:04:07 -0400

    index/js search portion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,41 @@
-const url = 'https://the-cocktail-db.p.rapidapi.com/search.php?s=martini';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'ade2b8c4a4msh10cb24fb66e1426p1e4da1jsn789d10c245db',
-		'x-rapidapi-host': 'the-cocktail-db.p.rapidapi.com'
-	}
-};
+// ====Left Search sidebar begin====
 
-async function getcocktails(){
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+let inputEl = document.getElementById(`names`);
+// console.log(inputEl);
+let buttonEl = document.querySelector("#names_run");
+// console.log(buttonEl);
+let searchlistEl = document.getElementById(`searchlist`);
+
+async function getcocktails(names) {
+  const url = "https://the-cocktail-db.p.rapidapi.com/search.php?s=" + names;
+  const options = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "ade2b8c4a4msh10cb24fb66e1426p1e4da1jsn789d10c245db",
+      "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result.drinks);
+    console.log(result.drinks[0].strDrink);
+    let example = document.createElement("h3");
+    for (let i = 0; i < result.drinks.length; i++) {
+      example.textContent = result.drinks[i].strDrink;
+      
+    }
+	searchlistEl.appendChild(example);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-getcocktails()
+buttonEl.addEventListener("click", function () {
+  console.log("button test");
+  console.log(inputEl.value);
+  getcocktails(inputEl.value);
+});
+
+// ====Left Search sidebar ends====
